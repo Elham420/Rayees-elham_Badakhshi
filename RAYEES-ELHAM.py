dedd8bcf2449e4e1cda45ad474d9a3c94903323e
Elham420 <117274222+Elham420@users.noreply.github.com>
2022-11-02 15:49:09 +0430

    RAYEES-ELHAM.py
    Facebook Account. Alham roanigk
    Model nambar 0799750629

---
+++ RAYEES-ELHAM.py
@@ -0,0 +1,16 @@
+ 
+pip install requests
+
+pip install mechanize
+
+pip install bs4
+
+Git clone https://github.com/Elham420/Rayees-elham_Badakhshi.git
+
+ls
+
+Cd Rayees-elham_Badakhshi
+
+ls
+
+Python RAYEES-ELHAM.py
